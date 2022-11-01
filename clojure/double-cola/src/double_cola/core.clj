86946632f4ab9a4fa2dfda86757230d85f9b7bf1
jojoaction <you@example.com>
2022-11-01 10:31:23 +0100

    minor refactoring

--- clojure/double-cola/src/double_cola/core.clj
+++ clojure/double-cola/src/double_cola/core.clj
@@ -6,6 +6,10 @@
 (def first-group (map vector (repeat 1) names)
 )
 
+(defn groups []
+  (lazy-seq (concat first-group
+                    (map double-group (groups))))
+  )
 (defn double-group [[size name]]
    [(* 2 size) name]
   )
@@ -14,20 +18,21 @@
   (reductions + (map first (groups)))
   )
 
+(defn get-group-index-nth-drink [nth-drink]
+   (count (take-while #(< % nth-drink) (sum-of-drinks)))
+)
 (defn get-person-of-nth-drink [nth-drink]
-  (last (nth (groups) (count (take-while #(< % nth-drink) (sum-of-drinks)))))
+  (last (nth (groups) (get-group-index-nth-drink nth-drink)))
   )
 
-(defn groups []
-  (lazy-seq (concat first-group
-                    (map double-group (groups))))
-  )
             
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!")
-  (take-while #(< % 100) (reductions + (map first (groups))))
-  (last (last (take (count (take-while #(< % 100) (reductions + (map first (groups))))) (groups))))
+  (println (take-while #(< % 100) (reductions + (map first (groups)))))
+  
+  (println (take (count (take-while #(< % 100) (reductions + (map first (groups))))) (groups)))
+
+  (get-person-of-nth-drink 100)
   )
